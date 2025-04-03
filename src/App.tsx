6c9f7df0cd8553e3f5a685988ce34c6dff034631
Ekaterina Fedyakina <katharine.fed05@yandex.ru>
2025-04-03 23:43:30 +0300

    Added scss and _variables in project. Setting alias.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import "@app/styles/global.scss";
+
 function App() {
   return (
     <>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@app": path.resolve(__dirname, "src/app"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        additionalData: `@use "@app/styles/_variables.scss" as *;
+                         @use "@app/styles/reset.scss" as *;`,
+      },
+    },
+  },
+});
